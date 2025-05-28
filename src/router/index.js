72fc8510a1f3aa8d2ac444f45cc1dfcb4afd2d6e
kromate <akpananthony33@gmail.com>
2025-05-28 21:45:38 +0100

    root

--- src/router/index.js
+++
@@ -1,8 +0,0 @@
-const routes = [
-  // ... your existing routes ...
-  {
-    path: '/:pathMatch(.*)*',
-    name: 'NotFound',
-    component: () => import('@/views/NotFound.vue') // or your 404 page component
-  }
-] 
